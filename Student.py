6e6bef0b306a4063cf96392c29d67b3c0231d963
SANJU S <sanjaysudi32@gmail.com>
2026-01-02 22:54:46 +0530

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,13 @@
+# Step 1: Store one student
+name = input("Enter student name: ")
+college = input("Enter college name: ")
+course = input("Enter course name: ")
+percentage = input("Enter percentage: ")
+
+student = [name,
+                 college, 
+                 course, 
+                 percentage]
+
+print("\nStudent Record:")
+print(student)
