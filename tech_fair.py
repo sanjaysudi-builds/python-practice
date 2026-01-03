41a385c93be4a4ad02e1ed41393a0096d10eb1ac
SANJU S <sanjaysudi32@gmail.com>
2026-01-03 21:12:07 +0530

    Add files via upload

---
+++ tech_fair.py
@@ -0,0 +1,21 @@
+# Program about attending a fair
+
+fair_name = "College Tech Fair"
+location = "Bagalkote"
+friends = ["Ravi", "Anita", "Kiran"]
+expenses = 1500
+
+print("I attended a fair.")
+print("Fair Name:", fair_name)
+print("Location:", location)
+print("Friends with me:", friends)
+print("Total expenses:", expenses, "rupees")
+print("I enjoyed the fair very much!")
+
+Output:
+I attended a fair.
+Fair Name: College Tech Fair
+Location: Bagalkote
+Friends with me: ['Ravi', 'Anita', 'Kiran']
+Total expenses: 1500 rupees
+I enjoyed the fair very much!
