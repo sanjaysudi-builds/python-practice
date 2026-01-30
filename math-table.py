771c962ae3472c683a0b4615d5c5b79fe60a38e8
SANJU S <sanjaysudi32@gmail.com>
2026-01-30 17:07:21 +0530

    Added 1 to 100 math table program

---
+++ math-table.py
@@ -0,0 +1,11 @@
+for i in range(1, 101):          # Tables from 1 to 100
+        print(f"\nMultiplication Table of {i}")
+        print("-" * 25)
+        for j in range(1, 11):       # Multiply from 1 to 10
+            print(f"{i} x {j} = {i * j}")
+    
+                   
+                   
+                   
+            
+    
